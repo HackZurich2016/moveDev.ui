6196dccd1b56c909b07d6f34b66fb1f4c5de5808
Paweł Buchowski <pawel.buchowski@refactory.net>
2016-09-17 20:29:12 +0200

    cleanup bootstrap; add masterpage; add login page

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from flask import Response
 from flask import render_template
 import json
 
-
 app = Flask(__name__, static_folder='static', static_url_path='')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @app.route('/bootstrap')
 def bootstrap():
     return render_template('bootstrap.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
