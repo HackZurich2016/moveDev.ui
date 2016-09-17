ac988daa79aabd252d00dee5c8b282e2c91fb703
disperate <julian.bigler@hotmail.com>
2016-09-17 14:15:15 +0200

    added bootstrap

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/bootstrap')
+def bootstrap():
+    return render_template('bootstrap.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
