54f513d0ab3eb859a091449a8b7c1407e62b16e1
disperate <julian.bigler@hotmail.com>
2016-09-17 14:09:00 +0200

    Load Gamestate from api

--- static/script.js
+++ static/script.js
@@ -1,26 +1,63 @@
-var gamestate = {
-	credits: 12,
-	enemyFields:[
-	],
-	myFields: [
-	]
+var gamestate;
+var URLgetGameState = "http://192.168.43.118:8000/game";
+//var URLgetGameState = "http://6fc04216.ngrok.io";
+
+function loadGameState(){
+
+	$.ajax({
+	    type: "GET",
+	    url: URLgetGameState,
+		dataType: 'json',
+	    success: function( data ) {
+			console.log("Gamestate Successful loaded");
+			gamestate = data;
+			redrawGameField();
+		},
+	    error: function (xhr, ajaxOptions, thrownError) {
+	      console.log(xhr.status);
+	      console.log(thrownError);
+	    }
+	})
 
 }
 
+function redrawGameField(){
+	$("#gameField").empty();
+	var x_max = 10;
+    var y_max = 10;
+    for (var x = 0; x_max > x; x++) {
+    	for (var y = 0; y_max > y; y++) {
+    		fieldValue = gamestate[x][y];
+        	$("#gameField").append('<div class="field" x="' + x + '" y="' + y +'">' + fieldValue + '</div>');
+    	};
+	};
+
+}
 
 $( document ).ready(function() {
- initGameField();
- $(".field")
- 	.mouseenter(function() {
-    	$( this ).css('background-color', 'red');
-  	})
-  	.mouseleave(function() {
-    	$( this ).css('background-color', '');
-  	})
-  	.click(function(){
-  		fieldClicked(this);
-  	});
-});
+	loadGameState();
+	
+	 $(".field")
+	 	.mouseenter(function() {
+	    	$( this ).css('background-color', 'red');
+	  	})
+	  	.mousedown(function() {
+	  		senderId = $( this ).id;
+
+	    	$( this ).css('background-color', '');
+	  	})
+	  	.mouseup(function() {
+	  		recieverId = $( this ).id;
+	    	$( this ).css('background-color', '');
+	  	})
+	  	.click(function(){
+	  		fieldClicked(this);
+	  	});
+	});
+
+function transfer(){
+	$recieverId
+}
 
 
 
@@ -35,16 +72,5 @@ function fieldClicked(sender){
   		$("#credits").html(gamestate.credits);
 	}
 	
-}
-
-function initGameField(){
-
-    var x_max = 10;
-    var y_max = 10;
-    for (var x = 0; x_max > x; x++) {
-    	for (var y = 0; y_max > y; y++) {
-        	$("#gameField").append('<div class="field" x="' + x + '" y="' + y +'"></div>');
-    	};
-	};
 }
 
